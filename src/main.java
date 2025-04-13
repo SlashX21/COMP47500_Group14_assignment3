0e01ef00802358dcabeb5b9be388476190c4e7cb
Clarence Chong <72379448+SlashX21@users.noreply.github.com>
2025-04-13 11:37:37 +0100

    Final Ammendment

---
+++ src/main.java
@@ -0,0 +1,8 @@
+import test.PerformanceTest;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Starting performance comparison of HashMap and LinkedHashMap...");
+        PerformanceTest.main(args);
+    }
+}
